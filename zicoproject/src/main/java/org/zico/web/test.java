633158f1d46bb4ca4cfb6623b37a28278bf3b874
water4000 <W>
2017-12-15 15:59:01 +0900

    basic

---
+++ zicoproject/src/main/java/org/zico/web/test.java
@@ -0,0 +1,15 @@
+package org.zico.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class test {
+	
+	@GetMapping("/dashboard")
+	public void test() {
+		
+	};
+	
+	
+}
